461b50d6353b1d7d0f365953b0431e067b50d690
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-06-24 11:26:47 +0300

    Add async increment button

--- sagas.js
+++ sagas.js
@@ -1,3 +1,22 @@
-export function* helloSaga() {
+import { delay } from 'redux-saga'
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+function* helloSaga() {
   console.log('Hello Sagas!');
+}
+
+function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
 }
